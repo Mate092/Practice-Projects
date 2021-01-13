5141f6cc38e6ba428efbeb3d9b01728cab706176
Máté Hidvégi <37485763+Mate092@users.noreply.github.com>
2021-01-13 16:30:03 +0000

    Add files via upload

---
+++ Weather app/main.js
@@ -0,0 +1,53 @@
+const api = {
+    key: "09e9e2837e08d27c6997cfa4a6c6b856",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchBox = document.querySelector('.search-box');
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchBox.value);
+    console.log(searchBox.value);};
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+
+    }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max}°c`
+}
+
+function dateBuilder (d) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sun","Mon","Tues","Wed","thurs","Fri","Sat"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
+    
